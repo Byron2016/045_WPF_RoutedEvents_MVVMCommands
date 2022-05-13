c12a0cd0ee0ef69537436aaa66c774d86f8440c1
Byron 2016 <bgva2005@yahoo.com>
2022-05-13 11:39:23 -0500

    feat: 003 IE add Models/ObservableObject class

---
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/Models/ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace aab_EventCommandsMVVM.Models
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged; //generado al implementar.
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
