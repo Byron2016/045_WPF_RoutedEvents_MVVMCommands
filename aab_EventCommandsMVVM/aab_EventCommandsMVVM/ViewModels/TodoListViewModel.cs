e56b1126321231f3488541e36fce2845d3301f59
Byron 2016 <bgva2005@yahoo.com>
2022-05-13 12:02:05 -0500

    feat: 003 IE add prop _todoItems to TodoListViewModel class

--- aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/TodoListViewModel.cs
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/TodoListViewModel.cs
@@ -1,8 +1,22 @@
 ﻿using aab_EventCommandsMVVM.Models;
+using System.Collections.ObjectModel;
 
 namespace aab_EventCommandsMVVM.ViewModels
 {
     public class TodoListViewModel : ObservableObject
     {
-    }
+        private ObservableCollection<TodoItem> _todoItems ;
+
+        public ObservableCollection<TodoItem> TodoItems
+		{
+            get { 
+				return _todoItems ; 
+			}
+            set { 
+				_todoItems  = value; 
+				OnPropertyChanged(nameof(TodoItems));
+			}
+        }
+
+	}
 }
