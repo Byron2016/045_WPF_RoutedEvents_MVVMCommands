80a4d2a96cb1ae2dc2e48a8b93b26bc35c29f7b9
Byron 2016 <bgva2005@yahoo.com>
2022-05-13 21:21:08 -0500

    feat: 2N IE add Models/TodoItem class

---
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aab_EventCommandsMVVM.Models
+{
+    public class TodoItem
+    {
+        public string Description { get; set; }
+        public string OwnerName { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
