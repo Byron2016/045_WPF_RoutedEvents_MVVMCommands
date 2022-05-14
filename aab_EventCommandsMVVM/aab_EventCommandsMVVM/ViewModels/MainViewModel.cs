a0f2c8b8a10da79c9107396f2cea547b284a49b0
Byron 2016 <bgva2005@yahoo.com>
2022-05-13 21:32:03 -0500

    feat: 2N IE add prop _todoListViewModel to MainViewModel class

--- aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/MainViewModel.cs
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/MainViewModel.cs
@@ -9,5 +9,18 @@ namespace aab_EventCommandsMVVM.ViewModels
 {
     public class MainViewModel: ObservableObject
     {
+        private TodoListViewModel _todoListViewModel;
+
+        public TodoListViewModel TodoListViewModel
+        {
+            get { return _todoListViewModel; }
+            set { _todoListViewModel = value; }
+        }
+
+        public MainViewModel(TodoListViewModel todoListViewModel)
+        {
+            _todoListViewModel = todoListViewModel;
+        }
+
     }
 }
