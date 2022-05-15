db0634ae4a4fa7623ca825052083f21158aa802a
Byron 2016 <bgva2005@yahoo.com>
2022-05-14 19:00:21 -0500

    feat: 2N IE 2

---
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using aab_EventCommandsMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aab_EventCommandsMVVM.ViewModels
+{
+    public class MainViewModel: ObservableObject
+    {
+        private TodoListViewModel _todoListViewModel;
+
+        public TodoListViewModel TodoListViewModel
+        {
+            get { 
+                return _todoListViewModel; 
+            }
+            set { 
+                _todoListViewModel = value; 
+                OnPropertyChanged(nameof(TodoListViewModel));
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            TodoListViewModel = new TodoListViewModel();
+        }
+    }
+}
---
+++ aab_EventCommandsMVVM/aab_EventCommandsMVVM/ViewModels/TodoListViewModel.cs
@@ -0,0 +1,37 @@
+﻿using aab_EventCommandsMVVM.Commands;
+using aab_EventCommandsMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace aab_EventCommandsMVVM.ViewModels
+{
+    public class TodoListViewModel: ObservableObject
+    {
+        public string Title { get; set; }
+
+        private ObservableCollection<TodoItem> _todoItems;
+
+        public ObservableCollection<TodoItem> TodoItems
+        {
+            get { return _todoItems; }
+            set { 
+                _todoItems = value; 
+                OnPropertyChanged(nameof(TodoItems));
+            }
+        }
+
+        public ICommand LoadTodoItemsCommand { get; set; }
+
+        public TodoListViewModel()
+        {
+            Title = "Titulo";
+            LoadTodoItemsCommand = new LoadTodoItemsCommand(this);
+            LoadTodoItemsCommand.Execute(null);
+        }
+    }
+}
